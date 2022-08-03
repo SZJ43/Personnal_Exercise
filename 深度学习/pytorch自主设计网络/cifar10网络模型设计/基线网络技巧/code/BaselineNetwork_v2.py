ca6b55e64824893e33b1d3dc866d7f21f448ef92
SZJ43 <64791841+SZJ43@users.noreply.github.com>
2022-08-03 11:42:55 +0800

    Update BaselineNetwork_v2.py

--- 深度学习/pytorch自主设计网络/cifar10网络模型设计/基线网络技巧/code/BaselineNetwork_v2.py
+++ 深度学习/pytorch自主设计网络/cifar10网络模型设计/基线网络技巧/code/BaselineNetwork_v2.py
@@ -12,7 +12,7 @@ import torch.nn.functional as F
 transform = transforms.Compose(
     [
         transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=False, transform=transform)
